fb31fc847d4e5d7dcb84e0b49c1b115d2c41a11f
Fernando Paiva <fernandopaiva@techamazon.tech>
2023-04-12 17:34:12 -0300

    modifica scripts usados para rodar o container da aplicação

--- application/controllers.go
+++ application/controllers.go
@@ -10,31 +10,9 @@ type controllers struct{}
 
 var Controllers controllers
 
-func (*controllers) Welcome(context echo.Context) error {
-	return context.String(
-		200,
-		"QR Code Generator API v1.2.2\n\nUsage: \n  [...]/qrcode/:text (default 512px)\n  [...]/qrcode/:text/:size",
-	)
-}
-
-func (*controllers) EncodeText(context echo.Context) error {
-	textToBeEncoded := context.Param("text")
-
-	qrcodePng, err := textToPng(textToBeEncoded, 512)
-
-	if err != nil {
-		return context.String(
-			500,
-			"Error encoding text => "+err.Error(),
-		)
-	}
-
-	return context.Blob(200, "image/png", qrcodePng)
-}
-
 func (controllers *controllers) EncodeTextWithSize(context echo.Context) error {
-	textToBeEncoded := context.Param("text")
-	qrcodeImageSize, _ := strconv.ParseUint(context.Param("size"), 10, 16)
+	textToBeEncoded := context.QueryParam("text")
+	qrcodeImageSize, _ := strconv.ParseUint(context.QueryParam("size"), 10, 16)
 
 	qrcodePng, err := textToPng(textToBeEncoded, qrcodeImageSize)
 
--- application/routes.go
+++ application/routes.go
@@ -1,9 +1,5 @@
 package application
 
 func (server *server) LoadRoutes() {
-	routesGroup := server.Instance.Group("/qrcode")
-
-	routesGroup.GET("/", Controllers.Welcome)
-	routesGroup.GET("/:text", Controllers.EncodeText)
-	routesGroup.GET("/:text/:size", Controllers.EncodeTextWithSize)
+	server.Instance.GET("/qrcode", Controllers.EncodeTextWithSize)
 }
